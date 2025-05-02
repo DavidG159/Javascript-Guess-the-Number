789f95b04ab9cf22bc9cbc89718232bdb7782980
DavidG159 <gaspardavid235@gmail.com>
2025-05-01 21:36:31 -0600

    Fixed the score

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 'use strict';
-
+let score = 20;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click',
     function () {
         const guess = Number(document.querySelector('.guess').value);
-        const score = Number(document.querySelector('.score').);
+
 
         if (!guess) {
             document.querySelector('.message').textContent = 'No Number! 😒';
@@ -14,16 +13,14 @@ document.querySelector('.check').addEventListener('click',
             document.querySelector('.message').textContent = 'Correct Number! 🎉 ';
         } else if (guess > secretNumber) {
             document.querySelector('.message').textContent = 'Number Too High!';
-            console.log(score);
-            //score - 1;
-            console.log('Test2');
-            document.querySelector('.score').textContent = score;
 
+            score = score - 1;
+
+            document.querySelector('.score').textContent = score;
+            console.log(score);
         } else if (guess < secretNumber) {
             document.querySelector('.message').textContent = 'Number Too Low...';
-            console.log('Test');
-            score - 1;
-            console.log('Test2');
+            score = score - 1;
             document.querySelector('.score').textContent = score;
         }
     });
